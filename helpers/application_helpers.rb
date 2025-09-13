319bd814285151b222dad23d315d9e9f45277f56
EmAreAitch <dev.emareaitch@gmail.com>
2025-09-13 21:46:11 +0530

    Added cms for testimonials and stats

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -1,5 +1,6 @@
 module ApplicationHelpers
   def responsive_image(path, fm: "avif", fit: nil, w: nil, h: nil, position: nil, q: 50)
+    return nil unless path.present?
     path = path.data.cover_image || "default_cover_image.png" if path.is_a? Middleman::Blog::BlogArticle
     
     if development?      
